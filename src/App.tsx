a583f2eac1f4e461c1687cac6fc9ed95b966e503
NahyunLeylaLee <florence5107@gmail.com>
2025-07-16 22:52:37 -0700

    add url

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import TV from "./Routes/TV";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
